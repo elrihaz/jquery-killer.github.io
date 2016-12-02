9043a2eb7ad4cc9a736e0ccacbe06bd37ac66498
elrihaz <elrihaz@gmail.com>
2016-12-02 17:47:41 +0100

    Implementation du menu en jquery + une seule page chargee avec mise à jour du div pour le theme voulu

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 $(document).ready(function () {
-  $('.cube').mouseenter(function () {
-    $(this).animate({
-      height: '+=10px'
-    });
-  });
-  $('.cube').mouseleave(function () {
-    $(this).animate({
-      height: '-=10px'
-    });
-  });
-  $('.cube').click(function () {
-    $(this).toggle(1000);
+
+  // Par défaut : affiche la présentation ds le div "page"
+  $("#page").load("EmmanuelLautréamont.html");
+
+  // Affiche le thème cliqué ds le div "page"
+  $('ul li').click(function(){
+    var lien=$(this).text(); // recup val de li
+    lien = lien.replace(/\s+/g, '') + ".html"; // suppr des espaces + .html
+    $("#page").load(lien);
   });
 });
