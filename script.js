31200969a071f540d1ac88de6398602d834cc9d1
elrihaz <elrihaz@gmail.com>
2016-12-03 12:08:17 +0100

    Mise à jour du theme actif qd clic sur le menu

--- script.js
+++ script.js
@@ -3,10 +3,14 @@ $(document).ready(function () {
   // Par défaut : affiche la présentation ds le div "page"
   $("#page").load("EmmanuelLautréamont.html");
 
-  // Affiche le thème cliqué ds le div "page"
+  // Clic sur le menu
   $('ul li').click(function(){
+    // Affiche le thème cliqué ds le div "page"
     var lien=$(this).text(); // recup val de li
-    lien = lien.replace(/\s+/g, '') + ".html"; // suppr des espaces + .html
+    lien = lien.replace(/\s+/g, '') + ".html"; // suppr des espaces et ajout de .html
     $("#page").load(lien);
+    // Mise à jour du theme actif ds le menu
+    $('ul li').removeClass("active");
+    $(this).addClass("active");
   });
 });
