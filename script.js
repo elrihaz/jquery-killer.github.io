30acd946206e19e255528f06511dd34b862cbf1b
elrihaz <elrihaz@gmail.com>
2016-12-21 09:35:50 +0100

    id page devient main et espace apres texte p

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
 
-  // Par défaut : affiche la présentation ds le div "page"
-  $("#page").load("EmmanuelLautréamont.html");
+  // Par défaut : affiche la présentation ds le div "main"
+  $("#main").load("EmmanuelLautréamont.html");
 
   // Clic sur le menu
   $('ul li').click(function(){
     // Affiche le thème cliqué ds le div "page"
     var lien=$(this).text(); // recup val de li
     lien = lien.replace(/\s+/g, '') + ".html"; // suppr des espaces et ajout de .html
-    $("#page").load(lien);
+    $("#main").load(lien);
     // Mise à jour du theme actif ds le menu
     $('ul li').removeClass("active");
     $(this).addClass("active");
